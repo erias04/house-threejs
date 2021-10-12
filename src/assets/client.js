568a25b7164f20a0041b8e3b9fd56a79b3cd8569
erias04 <lichtbargeld@gmail.com>
2021-10-12 22:46:57 +0200

    added root script with 3d-house.gltf

---
+++ src/assets/client.js
@@ -0,0 +1,136 @@
+import { GLTFLoader } from '../threejs/GLTFLoader.min.js';
+import { OrbitControls } from '../threejs/OrbitControls.min.js'
+
+// Camera configuration
+const camera = new THREE.PerspectiveCamera(
+    30,
+    window.innerWidth / window.innerHeight,
+    1,
+    1500,
+);
+// Camera poosition
+camera.position.set(-35, 70, 100);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+// Window Resize Handling
+export function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize);
+
+// Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xbfd1e5);
+
+// MapControls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+export function animate() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+
+// ambient light
+let hemiLight = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(hemiLight);
+
+// directional light
+let dirLight = new THREE.DirectionalLight(0xffffff, 1);
+dirLight.position.set(-30, 50, -30);
+scene.add(dirLight);
+dirLight.castShadow = true;
+dirLight.shadow.mapSize.width = 2048;
+dirLight.shadow.mapSize.height = 2048;
+dirLight.shadow.camera.left = -70;
+dirLight.shadow.camera.right = 70;
+dirLight.shadow.camera.top = 70;
+dirLight.shadow.camera.bottom = -70;
+
+
+// Add house
+function createHouse() {
+    const loader = new GLTFLoader();
+
+    loader.load('/src/assets/3d-house/house.gltf', function(house){
+
+        const model = house.scene;
+    model.position.set(0, 0, 0);
+
+    model.scale.set(1, 1, 1);
+
+    //model.castShadow = true
+    //model.receiveShadow = true
+
+    scene.add(house.scene);
+    
+    model.userData.draggable = true;
+    model.userData.name = 'house';
+    },
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	});
+}
+
+function createLabel() {
+    let pos = {x: 10, y: 10, z: 10};
+
+    var map = new THREE.TextureLoader().load( '' );
+    const material = new THREE.SpriteMaterial( { map: map } );
+
+    const sprite = new THREE.Sprite( material );
+
+    sprite.position.set(pos.x, pos.y, pos.z)
+
+    sprite.userData.clickable = true;
+    sprite.userData.name = 'SPRITE';
+
+    scene.add( sprite );
+}
+
+const raycaster = new THREE.Raycaster();
+const clickMouse = new THREE.Vector2();
+const moveMouse = new THREE.Vector2();
+var draggable = new THREE.Object3D();
+
+window.addEventListener('click', event => {
+  // calculate mouse position in normalized device coordinates
+	// (-1 to +1) for both components
+
+	clickMouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	clickMouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+  // update the picking ray with the camera and mouse position
+	raycaster.setFromCamera( clickMouse, camera );
+
+  // calculate objects intersecting the picking ray
+	const found = raycaster.intersectObjects( scene.children );
+
+  if(found.length > 0 && found[0].object.userData.draggable) {
+    draggable = found[0].object
+    console.log(`found draggable ${draggable.userData.name}`)
+  }
+
+})
+
+createHouse()
+//createLabel()
+
+animate()
