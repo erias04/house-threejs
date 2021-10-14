01dd832c6cbf1d28b990c2bd9d3ac8efaba9e309
erias04 <lichtbargeld@gmail.com>
2021-10-14 22:47:55 +0200

    preload 360 image when scene is loaded completely

--- src/assets/client.js
+++ src/assets/client.js
@@ -108,6 +108,30 @@ function createHouse() {
 
 		console.log( status + '% loaded' );
 
+    if (status === 100) {
+      function preloadImages(array) {
+        if (!preloadImages.list) {
+            preloadImages.list = [];
+        }
+        var list = preloadImages.list;
+        for (var i = 0; i < array.length; i++) {
+            var img = new Image();
+            img.onload = function() {
+                var index = list.indexOf(this);
+                if (index !== -1) {
+                    // remove image from the array once it's loaded
+                    // for memory consumption reasons
+                    list.splice(index, 1);
+                }
+            }
+            list.push(img);
+            img.src = array[i];
+        }
+    }
+    
+    preloadImages(["/src/assets/output.JPG"]);
+    }
+
 	},
     function ( onLoad ) {
 
