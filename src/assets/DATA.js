81d933bcd19f35e74d678a2441dbbc5e35d82417
erias04 <lichtbargeld@gmail.com>
2021-10-15 19:09:29 +0200

    added: DATA.js to store camera position and everything else

---
+++ src/assets/DATA.js
@@ -0,0 +1,22 @@
+var DATA = {
+    "streetViewDots": [
+      {
+          "positionx": -15,
+          "positiony": 29.3,
+          "positionz": 57,
+          "camerapositionx": -35,
+          "camerapositiony": 70,
+          "camerapositionz": 120
+      },
+      {
+          "positionx": -15,
+          "positiony": 29.3,
+          "positionz": 57,
+          "camerapositionx": -35,
+          "camerapositiony": 70,
+          "camerapositionz": 120
+      }
+    ]
+}
+
+export {DATA};
