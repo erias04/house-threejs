73a6beb127bf32e417ea1ab8cf2ca637cbf260da
erias04 <lichtbargeld@gmail.com>
2021-10-16 15:51:35 +0200

    changed 360.js completely First, I removed the init function, after that I rewrote everything so it works without the function

--- src/assets/360.js
+++ src/assets/360.js
@@ -1,6 +1,5 @@
 import * as THREE from '/src/threejs/three.module.min.js';
 
-let camera, scene, renderer;
 
 let isUserInteracting = false,
     onPointerDownMouseX = 0, onPointerDownMouseY = 0,
@@ -8,82 +7,23 @@ let isUserInteracting = false,
     lat = 0, onPointerDownLat = 0,
     phi = 0, theta = 0;
 
-init();
-animate();
 
-function init() {
 
-    const container = document.getElementById('container');
+// Camera configuration
+const camera = new THREE.PerspectiveCamera(
+    100, 
+    window.innerWidth / window.innerHeight, 
+    1, 
+    1100
+);
 
-    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 1100);
+// Renderer
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-    scene = new THREE.Scene();
-
-    const geometry = new THREE.SphereGeometry(500, 60, 40);
-    // invert the geometry on the x-axis so that all of the faces point inward
-    geometry.scale(- 1, 1, 1);
-
-    const texture = new THREE.TextureLoader().load('/src/assets/output-min-1.JPG');
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-
-    const mesh = new THREE.Mesh(geometry, material);
-
-    scene.add(mesh);
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
-
-    container.style.touchAction = 'none';
-    container.addEventListener('pointerdown', onPointerDown);
-
-    document.addEventListener('wheel', onDocumentMouseWheel);
-
-    //
-
-    document.addEventListener('dragover', function (event) {
-
-        event.preventDefault();
-        event.dataTransfer.dropEffect = 'copy';
-
-    });
-
-    document.addEventListener('dragenter', function () {
-
-        document.body.style.opacity = 0.5;
-
-    });
-
-    document.addEventListener('dragleave', function () {
-
-        document.body.style.opacity = 1;
-
-    });
-
-    document.addEventListener('drop', function (event) {
-
-        event.preventDefault();
-
-        const reader = new FileReader();
-        reader.addEventListener('load', function (event) {
-
-            material.map.image.src = event.target.result;
-            material.map.needsUpdate = true;
-
-        });
-        reader.readAsDataURL(event.dataTransfer.files[0]);
-
-        document.body.style.opacity = 1;
-
-    });
-
-    //
-
-    window.addEventListener('resize', onWindowResize);
-
-}
-
+// Window resize Handling
 function onWindowResize() {
 
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -92,6 +32,84 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
+window.addEventListener('resize', onWindowResize);
+
+//Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xbfd1e5);
+
+// MapControls
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+// Render scene
+function animate() {
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    update();
+
+}
+
+function createSphere() {
+const geometry = new THREE.SphereGeometry(500, 60, 40);
+// invert the geometry on the x-axis so that all of the faces point inward
+geometry.scale(- 1, 1, 1);
+
+const texture = new THREE.TextureLoader().load('/src/assets/output-min-1.JPG');
+const material = new THREE.MeshBasicMaterial({ map: texture });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+}
+
+
+document.body.style.touchAction = 'none';
+document.addEventListener('pointerdown', onPointerDown);
+
+document.addEventListener('wheel', onDocumentMouseWheel);
+
+//
+
+document.addEventListener('dragover', function (event) {
+
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+
+});
+
+document.addEventListener('dragenter', function () {
+
+    document.body.style.opacity = 0.5;
+
+});
+
+document.addEventListener('dragleave', function () {
+
+    document.body.style.opacity = 1;
+
+});
+
+document.addEventListener('drop', function (event) {
+
+    event.preventDefault();
+
+    const reader = new FileReader();
+    reader.addEventListener('load', function (event) {
+
+        material.map.image.src = event.target.result;
+        material.map.needsUpdate = true;
+
+    });
+    reader.readAsDataURL(event.dataTransfer.files[0]);
+
+    document.body.style.opacity = 1;
+
+});
+
+
+
+
 
 function onPointerDown(event) {
 
@@ -140,12 +158,6 @@ function onDocumentMouseWheel(event) {
 
 }
 
-function animate() {
-
-    requestAnimationFrame(animate);
-    update();
-
-}
 
 function update() {
 
@@ -168,3 +180,7 @@ function update() {
     renderer.render(scene, camera);
 
 }
+
+createSphere();
+
+animate();
