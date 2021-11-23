73e494542b5321e16e32a1bf70cd9cc310dcab87
erias04 <lichtbargeld@gmail.com>
2021-11-23 20:55:57 +0100

    import threejs and Model

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
+import ReactDOM from 'react-dom';
+import { Canvas } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className="canvas-container">
+      <Canvas fromeloop='demand'>
+        <Model url='./model/house.gltf'/>
+      </Canvas>
     </div>
   );
 }
