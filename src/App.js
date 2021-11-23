d674cd302bd5e0a5fc0436363e8e6bf5f3b085c8
erias04 <lichtbargeld@gmail.com>
2021-11-23 21:11:06 +0100

    delete model from App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import './App.css';
 
 import ReactDOM from 'react-dom';
 import { Canvas } from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
 
 function App() {
   return (
     <div className="canvas-container">
-      <Canvas fromeloop='demand'>
-        <Model url='./model/house.gltf'/>
+      <Canvas>
       </Canvas>
     </div>
   );
