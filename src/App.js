ea94f67b6774292df2306205743df873746d8729
erias04 <lichtbargeld@gmail.com>
2021-11-23 23:07:50 +0100

    imported model and OrbitControls

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import Model from './components/Model'
 
+import { OrbitControls} from '@react-three/drei'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Environment } from '@react-three/drei'
 
 export default function App() {
   return (
-    <>
+    <div className="App">
       <Canvas>
+        <ambientLight intensity={.5} />
+        <directionalLight intensity={.5} position={[0, 0, 5]} />
         <Suspense fallback={null}>
           <Model />
         </Suspense>
+        <OrbitControls />
       </Canvas>
-    </>
+    </div>
   )
 }
