d9aba049aa85c58f7fd97fea5397cc94af2f0ec0
erias04 <lichtbargeld@gmail.com>
2021-11-24 00:35:15 +0100

    added axesHelper and camera properties

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 import Model from './components/Model'
 
-import { OrbitControls} from '@react-three/drei'
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
+import * as THREE from 'three';
+import { OrbitControls} from '@react-three/drei';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
 
 export default function App() {
+
   return (
     <div className="App">
-      <Canvas>
+      <Canvas camera={{ fov: 75, position: [0, 50, 100] }}>
         <ambientLight intensity={.5} />
         <directionalLight intensity={.5} position={[0, 0, 5]} />
         <Suspense fallback={null}>
           <Model />
         </Suspense>
         <OrbitControls />
+        <axesHelper args={[100, 100]} />
       </Canvas>
     </div>
   )
