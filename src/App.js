ef3cd4678421b313200263b714f45c5d0046027f
erias04 <lichtbargeld@gmail.com>
2021-11-25 15:26:10 +0100

    added cirle and useState statment for streetview

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import './App.css';
-import Model from './components/Model'
+import Model from './components/Model';
+import streetView from './components/StreetView';
 
+import { useState } from 'react';
 import * as THREE from 'three';
-import { OrbitControls} from '@react-three/drei';
+import { OrbitControls, Circle } from '@react-three/drei';
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 
 export default function App() {
 
+  const [streetView, setStreetView] = useState(false);
+
   return (
     <div className="App">
+      {streetView ? 
       <Canvas camera={{ fov: 75, position: [0, 50, 100] }}>
         <ambientLight intensity={.5} />
         <directionalLight intensity={.5} position={[0, 0, 5]} />
         <Suspense fallback={null}>
           <Model />
+          <Circle rotation={[-Math.PI / 2, 0, 0]} position={[-20, 1.5, 39]} onClick={(e) => console.log('click')}>
+            <meshPhongMaterial color="white" />
+          </Circle>
         </Suspense>
         <OrbitControls />
         <axesHelper args={[100, 100]} />
       </Canvas>
+      : 
+      <Canvas>
+        <streetView />
+      </Canvas>
+      }
     </div>
   )
 }
