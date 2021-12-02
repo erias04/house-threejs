3b7aa1a9786ed5faf0c0ac2ba3d179e8fe61b3e7
erias04 <lichtbargeld@gmail.com>
2021-12-03 00:19:31 +0100

    added onclick function

--- src/App.js
+++ src/App.js
@@ -10,32 +10,32 @@ import { Canvas } from '@react-three/fiber';
 
 export default function App() {
 
-  const [streetView, setStreetView] = useState(false);
+  const [streetView, setStreetView] = useState(true);
+
+  function streetViewClicked() {
+    setStreetView(false);
+    console.log('clicked')
+  }
 
   return (
     <div className="App">
-      {streetView ? 
-      <Canvas camera={{ fov: 75, position: [0, 50, 100] }}>
-        <ambientLight intensity={.5} />
-        <directionalLight intensity={.5} position={[0, 0, 5]} />
-        <Suspense fallback={null}>
-          <Model />
-          <Circle rotation={[-Math.PI / 2, 0, 0]} position={[-20, 1.5, 39]} onClick={(e) => console.log('clicked')}>
-            <meshPhongMaterial color="white" />
-          </Circle>
-        </Suspense>
-        <OrbitControls />
-        <axesHelper args={[100, 100]} />
-        <Suspense fallback={null}>
-          <StreetView />
-        </Suspense>
-      </Canvas>
-      : 
-      <Canvas camera={{ fov: 150, zoom: 1, near: 0.005, far: 1000 }}>
-        <Suspense fallback={null}>
-        <StreetView />
-      </Suspense>
-      </Canvas>
+      {streetView ?
+        <Canvas camera={{ fov: 75, position: [0, 50, 100] }}>
+          <ambientLight intensity={.5} />
+          <directionalLight intensity={.5} position={[0, 0, 5]} />
+          <Suspense fallback={null}>
+            <Model />
+            <Circle rotation={[-Math.PI / 2, 0, 0]} position={[-20, 1.5, 39]} onClick={(e) => streetViewClicked()}>
+              <meshPhongMaterial color="white" />
+            </Circle>
+          </Suspense>
+          <OrbitControls />
+          <axesHelper args={[100, 100]} />
+        </Canvas>
+        :
+          <Suspense fallback={null}>
+            <StreetView />
+          </Suspense>
       }
     </div>
   )
