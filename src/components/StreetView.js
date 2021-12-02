a172b63838ae6cd0cf54d403287a8a153061b781
erias04 <lichtbargeld@gmail.com>
2021-12-02 23:01:50 +0100

    rewrite StreetView from scratch

--- src/components/StreetView.js
+++ src/components/StreetView.js
@@ -1,10 +1,5 @@
+import { Sphere, useCubeTexture, CubeCamera, PerspectiveCamera } from '@react-three/drei'
+import * as THREE from 'three'
+import { TextureLoader } from 'three'
+import { extend, useLoader } from '@react-three/fiber'
 
-function StreetView() {
-    return (
-        <>
-            
-        </>
-    )
-}
-
-export default StreetView
